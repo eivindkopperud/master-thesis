c8365824a876ed822adc5fb87e5d494d2f8fee70
Eivind Kopperud <eivind.kop@gmail.com>
2022-02-01 15:59:19 +0100

    correct interactions

--- data-generation/src/main/scala/Main.scala
+++ data-generation/src/main/scala/Main.scala
@@ -1,8 +1,8 @@
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.catalyst.dsl.expressions.{DslAttr, StringToAttributeConversionHelper}
+import org.apache.spark.sql.{SparkSession, functions}
 import org.apache.spark.sql.expressions.Window
-import org.apache.spark.sql.functions.{col, lag, when}
+import org.apache.spark.sql.functions.{col, lag, last, when}
+import org.json4s.JsonDSL.int2jvalue
 
 
 
@@ -23,9 +23,14 @@ object Main extends App {
 
   val df = spark.read.csv("src/main/resources/fb-messages.csv").toDF("from","to","time")
   val w = Window.orderBy("from", "to", "time")
-  val leadDf = df
-  .withColumn("previous", when(col("to") === lag("to", 1).over(w), lag("time", 1).over(w)).otherwise(""))
-  .withColumn("closeness", when(col("previous") === "", "" ).otherwise(col("time") - col("previous")))
+  var leadDf = df
+    .withColumn("previous", when(col("to") === lag("to", 1).over(w), lag("time", 1).over(w)).otherwise(""))
+    .withColumn("closeness", when(col("previous") === "", "" ).otherwise(col("time") - col("previous")))
+    .withColumn("id", when(col("closeness") === "", functions.monotonically_increasing_id()))
+    .withColumn("edge id", last(col("id"), ignoreNulls = true).over(w.rowsBetween(Window.unboundedPreceding, 0)))
+    // https://stackoverflow.com/questions/67888618/get-min-and-max-from-values-of-another-column-after-a-groupby-in-pyspark
+    .groupBy("edge id").agg(functions.min("time").alias("from time"), functions.max("time").alias("to time"))
+
   leadDf.show()
 
 
