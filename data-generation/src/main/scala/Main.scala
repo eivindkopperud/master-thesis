03cc36991639c8492882c59cd728c9f8edbb1de4
Eivind Kopperud <eivind.kop@gmail.com>
2022-02-02 14:42:18 +0100

    get intervals of given threshold

--- data-generation/src/main/scala/Main.scala
+++ data-generation/src/main/scala/Main.scala
@@ -1,37 +1,21 @@
-import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{SparkSession, functions}
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions.{col, lag, last, when}
-import org.json4s.JsonDSL.int2jvalue
-
-
-
 
 
 object Main extends App {
   val spark = SparkSession.builder.master("local").getOrCreate
   var sc = spark.sparkContext
-
-  /*
-       Tried with windows on RDDs first, might try again now that I fixed the stupid sbt issues:)
-
-  val events = "src/main/resources/fb-messages.csv"
-  val eventRDD = CsvUtils
-    .textFileToRDDCaseClass(events, sc)
-    .asInstanceOf[RDD[TemporalEvent[BigDecimal]]]
-  */
-
+  val threshold = 500.0
   val df = spark.read.csv("src/main/resources/fb-messages.csv").toDF("from","to","time")
-  val w = Window.orderBy("from", "to", "time")
+  val window = Window.orderBy("from", "to", "time")
   var leadDf = df
-    .withColumn("previous", when(col("to") === lag("to", 1).over(w), lag("time", 1).over(w)).otherwise(""))
+    .withColumn("previous", when(col("to") === lag("to", 1).over(window), lag("time", 1).over(window)).otherwise(""))
     .withColumn("closeness", when(col("previous") === "", "" ).otherwise(col("time") - col("previous")))
-    .withColumn("id", when(col("closeness") === "", functions.monotonically_increasing_id()))
-    .withColumn("edge id", last(col("id"), ignoreNulls = true).over(w.rowsBetween(Window.unboundedPreceding, 0)))
-    // https://stackoverflow.com/questions/67888618/get-min-and-max-from-values-of-another-column-after-a-groupby-in-pyspark
-    .groupBy("edge id").agg(functions.min("time").alias("from time"), functions.max("time").alias("to time"))
+    .withColumn("id", when(col("closeness") === "" || col("closeness") > threshold, functions.monotonically_increasing_id()))
+    .withColumn("edgeId", last(col("id"), ignoreNulls = true).over(window.rowsBetween(Window.unboundedPreceding, 0)))
+    val tempDf = leadDf.groupBy("edgeId").agg(functions.min("time").alias("from time"), functions.max("time").alias("to time"))
+    val leadDf2 = leadDf.as("self1").join(tempDf.as("self2"), col("self1.id") === col("self2.edgeId"), "inner").select("from", "to", "from time", "to time")
 
-  leadDf.show()
-
-
+  leadDf2.show()
 }
