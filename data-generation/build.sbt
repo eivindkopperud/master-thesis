6e97a5b53ca19c6093b765bc45d9ac0896edf5d5
Eivind Kopperud <eivind.kop@gmail.com>
2022-02-03 09:30:00 +0100

    start of graph generation

--- data-generation/build.sbt
+++ data-generation/build.sbt
@@ -5,5 +5,8 @@ version := "0.1"
 // https://mvnrepository.com/artifact/org.apache.spark/spark-sql
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.2.0"
 
+// https://mvnrepository.com/artifact/org.apache.spark/spark-sql
+libraryDependencies += "org.apache.spark" %% "spark-graphx" % "3.2.0"
+
 
 scalaVersion := "2.13.8"
--- data-generation/src/main/scala/Main.scala
+++ data-generation/src/main/scala/Main.scala
@@ -1,21 +1,39 @@
-import org.apache.spark.sql.{SparkSession, functions}
+import org.apache.spark.graphx.{Edge, Graph, VertexId}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions.{col, lag, last, when}
+import org.apache.spark.sql.{SparkSession, functions}
 
 
 object Main extends App {
   val spark = SparkSession.builder.master("local").getOrCreate
-  var sc = spark.sparkContext
-  val threshold = 500.0
-  val df = spark.read.csv("src/main/resources/fb-messages.csv").toDF("from","to","time")
+  val threshold = 250.0
   val window = Window.orderBy("from", "to", "time")
-  var leadDf = df
+  val df = spark.read.csv("src/main/resources/fb-messages.csv")
+    .toDF("from", "to", "time")
     .withColumn("previous", when(col("to") === lag("to", 1).over(window), lag("time", 1).over(window)).otherwise(""))
-    .withColumn("closeness", when(col("previous") === "", "" ).otherwise(col("time") - col("previous")))
+    .withColumn("closeness", when(col("previous") === "", "").otherwise(col("time") - col("previous")))
     .withColumn("id", when(col("closeness") === "" || col("closeness") > threshold, functions.monotonically_increasing_id()))
     .withColumn("edgeId", last(col("id"), ignoreNulls = true).over(window.rowsBetween(Window.unboundedPreceding, 0)))
-    val tempDf = leadDf.groupBy("edgeId").agg(functions.min("time").alias("from time"), functions.max("time").alias("to time"))
-    val leadDf2 = leadDf.as("self1").join(tempDf.as("self2"), col("self1.id") === col("self2.edgeId"), "inner").select("from", "to", "from time", "to time")
+  val tempDf = df.groupBy("edgeId").agg(functions.min("time").alias("from time"), functions.max("time").alias("to time"))
+  val leadDf = df.as("self1").join(tempDf.as("self2"), col("self1.id") === col("self2.edgeId"), "inner").select("from", "to", "from time", "to time")
 
-  leadDf2.show()
+  val vertices: RDD[(VertexId, Long)] = leadDf
+    .select("from")
+    .distinct
+    .rdd.map(row => row.getAs[Long]("from"))
+    .zipWithIndex // associate a long index to each vertex
+    .map(_.swap)
+
+  val edges: RDD[Edge[(BigDecimal, BigDecimal)]] = leadDf
+    .select("from", "to", "from time", "to time")
+    .rdd.map(
+    row =>
+      row.getAs[Long]("from"),
+    row.getAs[String]("to"),
+    (row.getAs[BigDecimal]("from time"),
+      row.getAs[BigDecimal]("to time")))
+
+  val graph = Graph(vertices, edges)
+  println(graph.numEdges, graph.numVertices)
 }
