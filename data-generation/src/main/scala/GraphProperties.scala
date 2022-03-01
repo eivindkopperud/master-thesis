57daaecb6ced173e1dcb0ae7bad9bd890ea657c3
Theodor René Carlsen <theodor.rene@gmail.com>
2022-03-01 16:44:18 +0100

    Add function for outputting stats about graph

--- data-generation/src/main/scala/GraphProperties.scala
+++ data-generation/src/main/scala/GraphProperties.scala
@@ -8,7 +8,8 @@ import scala.reflect.ClassTag
 object GraphProperties {
 
 
-  //def getPowerLawExponent
+
+  //TODO def getPowerLawExponent
 
   /** Visualize the power law for node outdegreenees
    * Saves line diagram to file and opens it in a viewer
@@ -17,7 +18,7 @@ object GraphProperties {
    * @tparam VD VertexType
    * @tparam ED EdgeType
    */
-  def visPowerLaw[VD, ED](graph: Graph[VD, ED], graphName:String = "Graph", filename: String = "lines.png"): Unit = {
+  def visPowerLaw[VD, ED](graph: Graph[VD, ED], graphName: String = "Graph", filename: String = "lines.png"): Unit = {
     val g = graph.ops
     val f = Figure()
     val p = f.subplot(0)
@@ -36,14 +37,49 @@ object GraphProperties {
    * 1. This method respects edge direction. In the general sense of diameter this should be ignored.
    * 2. This scales exponentially or something like that. Get shortest path between all vertices and then return max path length
    *
+   * ClassTag is there so that the type ED is not gone at runtime. So called Type Erasure, that happens with the JVM
+   *
    * @param graph Graph
    * @tparam VD VertexType
    * @tparam ED EdgeType
    * @return diameter of graph
    */
-  def shortestPath[VD, ED: ClassTag](graph: Graph[VD, ED]): Int = {
+  def getDirectedDiameterOfGraph[VD, ED: ClassTag](graph: Graph[VD, ED]): Int = {
     val vertex_ids = graph.vertices.map(_._1).collect()
     ShortestPaths.run(graph, vertex_ids).vertices.mapValues(_.values.max).values.max()
   }
+
+  case class GraphStats(order: Long, size: Long, average_outdegree: Double)
+
+  /** ToString for GraphStats
+   *
+   * But I haven't learned idiomatic Scala yet. Think this should be done differently
+   * @param g GraphStats object
+   * @return Table format as a string
+   */
+  def graphStatsToString(g: GraphStats): String = {
+    s"""description\tvalue
+       |order\t${g.order}
+       |size\t${g.size}
+       |Average outdegree\t${g.average_outdegree}
+    """.stripMargin
+  }
+
+  /** Print general stats about graph
+   *
+   * TODO find a better datastructure, not very extensible right now
+   * TODO Add more stats (PowerLaw exponent etc)
+   * @param graph Graph
+   * @tparam VD VertexType
+   * @tparam ED EdgeType
+   */
+  def printStats[VD, ED](graph: Graph[VD, ED]): Unit = {
+    val graphStats = GraphStats(
+      graph.ops.numVertices,
+      graph.ops.numEdges,
+      graph.ops.outDegrees.values.sum() / graph.ops.numEdges)
+   println(graphStatsToString(graphStats))
+  }
+
 
 }
