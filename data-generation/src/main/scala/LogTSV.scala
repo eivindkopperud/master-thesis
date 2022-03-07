c96421770bf2d5861c63dfebefc05498caea23e2
Theodor René Carlsen <theodor.rene@gmail.com>
2022-03-07 14:27:17 +0100

    Finish LogTSV implementation
    - Add tests
    - Add writing to file
    - Add reading from file

--- data-generation/src/main/scala/LogTSV.scala
+++ data-generation/src/main/scala/LogTSV.scala
@@ -1,11 +1,14 @@
 import Action.{CREATE, DELETE, UPDATE}
 import LTSV.Attributes
+import com.github.javafaker.Faker
 
 import java.time.{Instant, LocalDateTime, ZoneOffset}
 import scala.util.Try
+import scala.util.Random.nextInt
+import scala.io.Source
 
-/**
- * This is apparently the best way to make enums in Scala2
+/** Action Enum
+ *
  */
 sealed abstract class Action
 object Action{
@@ -14,12 +17,13 @@ object Action{
   final case object DELETE extends Action
 }
 
-/**
+/** LogTSV
+ *
  * Everything is built around this case class
- * @param timestamp
- * @param action
- * @param objectId
- * @param attributes
+ * @param timestamp When did the log_entry happen?
+ * @param action Type of action
+ * @param objectId Id of the object
+ * @param attributes List of (Key,Value) attributes relevant to the entry
  */
 case class LogTSV(
                  timestamp : Instant,
@@ -29,67 +33,148 @@ case class LogTSV(
                  )
 
 object LTSV {
+
+  /** Random LogTSV instance
+   *
+   * Convenient for testing
+   * @return An instance of LogTSV
+   */
+  def randomLTSV():LogTSV = {
+    val actions = List(CREATE, UPDATE, DELETE)
+    val faker = new Faker()
+    LogTSV(
+      timestamp = Instant.now(),
+      action = actions(nextInt(actions.size)),
+      objectId = nextInt(10000),
+      attributes = List(("champion",faker.leagueOfLegends().champion()),
+        ("friends", faker.friends().character()))
+    )
+
+
+
+  }
+  // Type alias
   type Attributes = List[(String, String)]
-  def serializeLTSV(log_entry:LogTSV ): String ={
-    val timestamp = LocalDateTime.ofInstant(log_entry.timestamp, ZoneOffset.UTC).toString
-    val action = log_entry.action match {
+
+  /** Serialize a single LogTSV
+   *
+   * @param logEntry The log entry
+   * @return String representation of the LogTSV
+   */
+  def serializeLTSV(logEntry:LogTSV ): String ={
+    val timestamp = LocalDateTime.ofInstant(logEntry.timestamp, ZoneOffset.UTC).toString +"Z"
+    val action = logEntry.action match {
       case CREATE => "CREATE"
       case UPDATE => "UPDATE"
       case DELETE => "DELETE"
     }
-    val objectId = log_entry.objectId.toString
-    val attributes = serializeAttributes(log_entry.attributes)
+    val objectId = logEntry.objectId.toString
+    val attributes = serializeAttributes(logEntry.attributes)
+
     List(timestamp, action, objectId, attributes)
       .mkString("\t")
   }
 
-  /**
-   * TODO Prove that serializeList is the inverse of deserializeList with tests
-   * Serializing twice should equal NOP
-   * @param entries
-   * @return
-   */
   def serializeList(entries: List[LogTSV]):String = entries.map(serializeLTSV).mkString("\n")
   def deserializeList(entries: String):List[LogTSV] = entries.split("\n").flatMap(deserializeLTSV).toList
 
-  /**
+  /**  Deserialize a single LogTSV
+   *
    * Check this shit out. Monadic error handling
-   * All the variables with s_ prefix return an Option type
-   * If any return a None the whole block returns a None
+   *
+   * All the variables with s_ prefix return an Option type.
+   * If any return a None the whole block returns a None.
    * If every one of the are parsed correctly it return Some(LogTsv)
    *
-   * @param log_entry String representation of a log entry
+   * TODO Rewrite this into Either-type so that information on what is going wrong isn't lost
+   *
+   * @param logEntry String representation of a log entry
    * @return Possibly a LogTSV
    */
-  def deserializeLTSV(log_entry:String):Option[LogTSV] = {
-    val timestamp :: action :: objectid :: attributes: Seq[String] = log_entry.split('\t')
+  def deserializeLTSV(logEntry:String):Option[LogTSV] = {
+    // This line could fail, but its a hassle to make safe
+    // Destructures the first three items, and 'attributes' is the tail
+    val timestamp::action::objectId::attributes : List[String]= logEntry.split('\t').toList
     for {
-      s_timestamp <- Try {Instant.parse(timestamp)}.toOption
-      s_action <- action match {
+      deserializedTimestamp <- Try {Instant.parse(timestamp)}.toOption
+      deserializedAction <- action match {
         case "CREATE" => Some(CREATE)
         case "UPDATE" => Some(UPDATE)
         case "DELETE" => Some(DELETE)
         case _ => None
       }
-      s_objectId <- Try{objectid.toLong}.toOption
-      s_attributes <- deserializeAttributes(attributes)
-    } yield LogTSV(s_timestamp, s_action, s_objectId, s_attributes)
+      deserializedObjectId <- Try{objectId.toLong}.toOption
+      deserializedAttributes <- deserializeAttributes(attributes)
+    } yield LogTSV(deserializedTimestamp, deserializedAction, deserializedObjectId, deserializedAttributes)
   }
 
+
+  /** Serialize list of attributes
+   *
+   * Colon and tabs characters are filtered out since they are part of the deserializing process
+   *
+   * @param attributes List of (Key,Value) representation of each attributes
+   * @return String representation of the attributes
+   *
+   */
   def serializeAttributes(attributes:Attributes): String = {
-    val noTabs = (x:Char) => x != '\t'
+    val noTabsOrColon = (x:Char) => x != '\t' && x != ':'
     attributes.map(
-      tup => s"${tup._1}:${tup._2}"
-    ).map(_.filter(noTabs)).mkString("\t")
+      tup => s"${tup._1.filter(noTabsOrColon)}:${tup._2.filter(noTabsOrColon)}"
+    ).mkString("\t")
   }
+
+  /** Deserialize list of attributes
+   *
+   * @param attributes List of string representation of each attribute
+   * @return Possibly a list of parsed attributes
+   */
   def deserializeAttributes(attributes: List[String]) : Option[Attributes] = {
-    val handleSingleKeyVal = (x:List[String]) => (x(0), x(1))
-    if (attributes.length % 2 != 0) {
-      None
+    val x = attributes
+      .map(_.split(':').toList)
+      .map {
+        case key :: value :: Nil => Some(key, value)
+        case _ => None
+      }
+    if (x.forall(_.isDefined)){ // Did everything parse correctly?
+      Some(x.flatten)
     } else {
-      // Fuck this doesn't work. I want [1,2,3,4] ->  [1,2][2,3][3,4], not [1,2][2,3][3,4]
-      // @Eivind I know you have done this before
-      Some(attributes.sliding(2).map(handleSingleKeyVal).toList)
+      None
     }
+  }
+   def writeToFile(entries:List[LogTSV], filename:String="logs.tsv"):Unit = {
+     val pw = new java.io.PrintWriter("logs.tsv")
+     pw.write(serializeList(entries))
+     pw.close()
+   }
+  def readFromFile(filename:String="logs.tsv"):List[LogTSV] = {
+    val f = Source.fromFile(filename)
+    val list = deserializeList(f.mkString)
+    f.close()
+    list
+  }
+
+  /** Home made testsuite
+   *
+   * TODO set up a TestSuite for scala
+   *
+   * Generates some random TSVs and makes sure that serializing and deserializing
+   * are inverse functions in relation to each other
+   *
+   */
+  def tests():Unit = {
+    for( _ <- 1 to 10){
+      val tsv = randomLTSV()
+      val s_tsv = serializeLTSV(tsv)
+      val d_tsv = deserializeLTSV(s_tsv).get
+      val s_d_tsv = serializeLTSV(d_tsv)
+      assert(d_tsv == tsv)
+      assert(s_d_tsv == s_tsv)
+    }
+    val x = for (_ <- 1 to 5) yield randomLTSV()
+    assert(deserializeList(serializeList(x.toList)) == x.toList)
+
+    writeToFile(x.toList, filename="testLogs.tsv")
+    assert(readFromFile(filename = "testLogs.tsv") == x.toList)
   }
 }
