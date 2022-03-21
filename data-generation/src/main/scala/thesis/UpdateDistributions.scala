0b4438f45656dd22dda401b69d4c3ce8cbda59d5
Theodor René Carlsen <theodor.rene@gmail.com>
2022-03-21 11:53:14 +0100

    Sort edges without resorting to collect
    Also formatting, and remove unnecessary code

--- data-generation/src/main/scala/thesis/UpdateDistributions.scala
+++ data-generation/src/main/scala/thesis/UpdateDistributions.scala
@@ -1,21 +1,23 @@
 package thesis
 
-import Action.{CREATE, DELETE}
-import DistributionType.LogNormalType
-import factories.LogFactory
-
 import breeze.plot.{Figure, hist}
 import breeze.stats.distributions.{Gaussian, LogNormal, Uniform}
+import factories.LogFactory
 import org.apache.spark.SparkContext
 import org.apache.spark.graphx.{Edge, Graph, VertexId, VertexRDD}
 import org.apache.spark.rdd.RDD
+import thesis.Action.{CREATE, DELETE}
+import thesis.DistributionType.LogNormalType
+
 import scala.reflect.ClassTag
 
 sealed abstract class CorrelationMode
 
 object CorrelationMode {
   final case object Uniform extends CorrelationMode
+
   final case object PositiveCorrelation extends CorrelationMode
+
   final case object NegativeCorrelation extends CorrelationMode
 }
 
@@ -23,12 +25,15 @@ sealed abstract class DistributionType
 
 object DistributionType {
   final case class LogNormalType(mu: Int, sigma: Double) extends DistributionType
+
   final case class GaussianType(mu: Int, sigma: Double) extends DistributionType
-  final case class UniformType(low:Double, high: Double) extends DistributionType
+
+  final case class UniformType(low: Double, high: Double) extends DistributionType
 }
 
-final case class IntervalAndUpdateCount(interval:TimeInterval, count: Int)
-final case class IntervalAndDegrees(interval:TimeInterval, degree: Int)
+final case class IntervalAndUpdateCount(interval: TimeInterval, count: Int)
+
+final case class IntervalAndDegrees(interval: TimeInterval, degree: Int)
 
 object UpdateDistributions {
 
@@ -40,12 +45,12 @@ object UpdateDistributions {
    * @param graph        A graph
    * @param mode         Mode for distributing the weights based on vertex degree
    * @param distribution Some probability distribution
-   * @return             Processed graph
+   * @return Processed graph
    */
   def addVertexUpdateDistribution[VD: ClassTag, ED: ClassTag](sc: SparkContext, graph: Graph[VD, ED], mode: CorrelationMode, distribution: DistributionType): Graph[Int, ED] = {
-    val verticesWithDegree: RDD[(VertexId, Int)] = sortVertexByMode(graph.ops.degrees, mode)
+    val verticesWithDegree = sortVerticesByMode(graph.ops.degrees, mode)
 
-    val updates: RDD[Int] = graph.mapVertices((_, _) => getDistributionDraw(distribution))
+    val updates = graph.mapVertices((_, _) => getDistributionDraw(distribution))
       .vertices
       .sortBy(_._2)
       .map(_._2)
@@ -62,11 +67,19 @@ object UpdateDistributions {
    * @param mode Indicates the mode of which the nodes are ordered by
    * @return Sorted RDD
    * */
-  private def sortVertexByMode(vertices: VertexRDD[Int], mode: CorrelationMode): RDD[(VertexId, Int)] = {
+  private def sortVerticesByMode(vertices: VertexRDD[Int], mode: CorrelationMode): RDD[(VertexId, Int)] = {
     mode match {
       case CorrelationMode.Uniform => vertices.sortBy(_.hashCode())
       case CorrelationMode.PositiveCorrelation => vertices.sortBy(_._2, ascending = true)
       case CorrelationMode.NegativeCorrelation => vertices.sortBy(_._2, ascending = false)
+    }
+  }
+
+  private def sortEdgesByMode(edges: RDD[Edge[IntervalAndDegrees]], mode: CorrelationMode): RDD[Edge[IntervalAndDegrees]] = {
+    mode match {
+      case CorrelationMode.Uniform => edges.sortBy(_.hashCode())
+      case CorrelationMode.PositiveCorrelation => edges.sortBy(_.attr.degree, ascending = true)
+      case CorrelationMode.NegativeCorrelation => edges.sortBy(_.attr.degree, ascending = false)
     }
   }
 
@@ -77,53 +90,46 @@ object UpdateDistributions {
    * @param mode         Mode for distributing the weights based on vertex degree
    * @param distribution Some probability distribution
    * @return */
-  def addEdgeUpdateDistribution[VD](sc: SparkContext, graph: Graph[Int, TimeInterval], mode: CorrelationMode, distribution: DistributionType): Graph[Int, IntervalAndUpdateCount] = {
-
+  def addEdgeUpdateDistribution[VD](sc: SparkContext,
+                                    graph: Graph[Int, TimeInterval],
+                                    mode: CorrelationMode,
+                                    distribution: DistributionType): Graph[Int, IntervalAndUpdateCount] = {
+    // TODO remove this collect
     val vertexUpdateHashMap = graph
       .vertices
       .collect()
       .toMap
 
-    val edgesWithDegree = graph
-      .mapEdges(edge => IntervalAndDegrees(edge.attr,getVertexUpdateSum(edge, vertexUpdateHashMap)))
-      .edges
-      .collect()
-      .sortBy(edge => edge)(getEdgeSorting(mode)) // this can be rewritten the same way as addVertexUpdateDistribution
+    val edgesWithDegree = {
+      val edgesWithDegreeUnsorted = graph
+        .mapEdges(edge =>
+          IntervalAndDegrees(edge.attr, getVertexUpdateSum(edge, vertexUpdateHashMap)))
+        .edges
+      sortEdgesByMode(edgesWithDegreeUnsorted, mode)
+    }
 
     val numberOfUpdates = graph
       .mapEdges(_ => getDistributionDraw(distribution))
       .edges
-      .collect()
       .sortBy(edge => edge.attr)
       .map(edge => edge.attr)
 
-    val edgesWithUpdateCount = sc.parallelize(
-      edgesWithDegree
-        .zip(numberOfUpdates)
-        .map(edgeAndCount =>
-          Edge(edgeAndCount._1.srcId, edgeAndCount._1.dstId, IntervalAndUpdateCount(edgeAndCount._1.attr.interval, edgeAndCount._2)))
-    )
+    val edgesWithUpdateCount = edgesWithDegree
+      .zip(numberOfUpdates)
+      .map(edgeAndCount =>
+        Edge(edgeAndCount._1.srcId, edgeAndCount._1.dstId, IntervalAndUpdateCount(edgeAndCount._1.attr.interval, edgeAndCount._2)))
     Graph[Int, IntervalAndUpdateCount](graph.vertices, edgesWithUpdateCount)
   }
 
 
-  /**
+  /** Get the sum of the edges connected vertices
    *
-   * @param edge
+   * @param edge    relevant edge
    * @param hashMap Hash map with vertices as keys and update counts as values
    * @return The sum of updates of the edge's connected vertices
    */
   private def getVertexUpdateSum[ED](edge: Edge[ED], hashMap: Map[VertexId, Int]): Int = {
     hashMap.getOrElse(edge.srcId, 0) + hashMap.getOrElse(edge.dstId, 0)
-  }
-
-  private def getEdgeSorting(mode: CorrelationMode): Ordering[Edge[IntervalAndDegrees]] = {
-    type x = (TimeInterval, Int)
-    mode match {
-      case CorrelationMode.Uniform => (x: Edge[_], y: Edge[_]) => x.hashCode() compareTo y.hashCode() // We want no correlation so they are ordered randomly
-      case CorrelationMode.PositiveCorrelation => (x: Edge[IntervalAndDegrees], y: Edge[IntervalAndDegrees]) => x.attr.degree compareTo y.attr.degree
-      case CorrelationMode.NegativeCorrelation => (x: Edge[IntervalAndDegrees], y: Edge[IntervalAndDegrees]) => y.attr.degree compareTo x.attr.degree
-    }
   }
 
   /** Draw a number from a probability distribution
@@ -139,8 +145,8 @@ object UpdateDistributions {
     }
   }
 
-  private def getSortedUniformDistribution(low:Double, high:Double, numSamples:Int):List[Long] =
-    Uniform(low,high).sample(numSamples).map(_.toLong).toList.sorted
+  private def getSortedUniformDistribution(low: Double, high: Double, numSamples: Int): List[Long] =
+    Uniform(low, high).sample(numSamples).map(_.toLong).toList.sorted
 
   def generateLogs(graph: Graph[Int, IntervalAndUpdateCount]): RDD[LogTSV] = {
     val (vertices, edges) = (graph.vertices, graph.edges)
@@ -156,11 +162,11 @@ object UpdateDistributions {
 
     // Has to be rewritten as Edge[Int] should be Edge[(Int, Timestamp)] from existing data
     val edgeIdWithTimestamp = edges.map(edge => {
-      val distribution = getSortedUniformDistribution(edge.attr.interval.start.getEpochSecond,edge.attr.interval.stop.getEpochSecond, edge.attr.count)
-      ((edge.srcId, edge.dstId),(edge.attr.interval, distribution))
+      val distribution = getSortedUniformDistribution(edge.attr.interval.start.getEpochSecond, edge.attr.interval.stop.getEpochSecond, edge.attr.count)
+      ((edge.srcId, edge.dstId), (edge.attr.interval, distribution))
     })
     val edgeLogs = edgeIdWithTimestamp.map(edge => {
-      val (ids, (interval,timestamps)) = edge
+      val (ids, (interval, timestamps)) = edge
       val startTSV = LogFactory().getCreateDeleteEdgeTSV(ids, interval.start, CREATE)
       val endTSV = LogFactory().getCreateDeleteEdgeTSV(ids, interval.stop, DELETE)
       (ids, List(startTSV) ++ timestamps.map(LogFactory().generateEdgeTSV(ids, _)) ++ List(endTSV))
@@ -168,7 +174,6 @@ object UpdateDistributions {
 
     (vertexLogs ++ edgeLogs).sortBy(_.timestamp)
   }
-
 
 
   /** Add log normal distributed weights as the property for vertices and edges
@@ -180,12 +185,12 @@ object UpdateDistributions {
    * @param mu    Expected value
    * @param sigma Standard deviation
    */
-  def addLogNormalGraphUpdateDistribution[VD:ClassTag](sc: SparkContext, graph: Graph[VD, TimeInterval], mu: Int = 100, sigma: Double = 2): Graph[Int, IntervalAndUpdateCount] = {
+  def addLogNormalGraphUpdateDistribution[VD: ClassTag](sc: SparkContext, graph: Graph[VD, TimeInterval], mu: Int = 100, sigma: Double = 2): Graph[Int, IntervalAndUpdateCount] = {
     val g1 = addVertexUpdateDistribution(sc, graph, CorrelationMode.PositiveCorrelation, LogNormalType(mu, sigma))
     addEdgeUpdateDistribution(sc, g1, CorrelationMode.PositiveCorrelation, LogNormalType(mu, sigma))
   }
 
-  def getLogTSV[VD:ClassTag](sc:SparkContext, graph: Graph[VD, TimeInterval]): RDD[LogTSV] = {
+  def getLogTSV[VD: ClassTag](sc: SparkContext, graph: Graph[VD, TimeInterval]): RDD[LogTSV] = {
     val g = addLogNormalGraphUpdateDistribution(sc, graph)
     generateLogs(g)
   }
