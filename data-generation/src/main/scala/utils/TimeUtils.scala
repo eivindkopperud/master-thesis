640f4dc4339a8a37cfc6fc68f9a792c6875c8ecc
Theodor René Carlsen <theodor.rene@gmail.com>
2022-04-05 18:06:26 +0200

    Add spicy time utils function for implicit conversion

--- data-generation/src/main/scala/utils/TimeUtils.scala
+++ data-generation/src/main/scala/utils/TimeUtils.scala
@@ -3,6 +3,7 @@ package utils
 import breeze.stats.distributions.Uniform
 
 import java.time.Instant
+import scala.language.implicitConversions
 
 object TimeUtils {
   def getRandomOrderedTimestamps(amount: Int, startTime: Instant, endTime: Instant): Seq[Instant] = {
@@ -10,5 +11,23 @@ object TimeUtils {
       .sample(amount)
       .sortWith((t1, t2) => t1 < t2)
       .map(timestamp => Instant.ofEpochSecond(timestamp.toLong))
+  }
+
+  /** Convert Long to Instant
+   *
+   * If this function is in scope, everytime someone uses Long for a function that
+   * take Instant as a parameter, it will be converted
+   *
+   * getYearFromInstant(x:Instant):Int
+   * getYearFromInstant(0) !! Compilation fails
+   * import TimeUtils._
+   * getYearFromInstant(0) !! 0 get converted to type Instant
+   * Crazy stuff
+   *
+   * @param time
+   * @return Instant
+   */
+  implicit def secondsToInstant(time: Long): Instant = {
+    Instant.ofEpochSecond(time)
   }
 }
