3c7c97982b7d1b022bd4d12f1509c5ed39a0c5bc
Theodor René Carlsen <theodor.rene@gmail.com>
2022-04-22 15:12:46 +0200

    Add a simple way for deterministic runs in PossibleWorkFlow

--- data-generation/src/main/scala/thesis/PossibleWorkFlow.scala
+++ data-generation/src/main/scala/thesis/PossibleWorkFlow.scala
@@ -3,7 +3,9 @@ package thesis
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SparkSession
 import org.slf4j.LoggerFactory
-import thesis.LTSV.RDDLogTSVMethod
+import thesis.DataSource.FbMessages
+import thesis.DistributionType.UniformType
+import thesis.LTSV.{RDDLogTSVMethod, readFromFile}
 import thesis.SparkConfiguration.getSparkSession
 import thesis.TopologyGraphGenerator.generateGraph
 import thesis.UpdateDistributions.{addGraphUpdateDistribution, generateLogs}
@@ -19,26 +21,31 @@ object PossibleWorkFlow {
     // https://www.theguardian.com/info/developer-blog/2016/dec/22/parental-advisory-implicit-content
     implicit val spark: SparkSession = getSparkSession
     implicit val sc: SparkContext = spark.sparkContext
-    sc.setLogLevel("WARN") // I can't get the log4j.properties file to have any effect
+    //sc.setLogLevel("WARN") // I can't get the log4j.properties file to have any effect
     val logger = LoggerFactory.getLogger("PossibleWorkFlow")
     val start = System.nanoTime()
 
-    logger.warn("Generate inital graph from dataset")
-    val graph = generateGraph(1)
+    val usePrevious = true // Set to true if you want to be deterministic in the runs
+    val logs = if (usePrevious) {
+      logger.warn("Using previous logs")
+      sc.parallelize(readFromFile("previous_run.tsv"))
+    } else {
+      logger.warn("Generate inital graph from dataset")
+      val graph = generateGraph(2, dataSource = FbMessages)
 
-    logger.warn("Augment the dataset with updates")
-    val g = addGraphUpdateDistribution(graph)
+      logger.warn("Augment the dataset with updates")
+      val g = addGraphUpdateDistribution(graph, UniformType(0, 2))
 
-    logger.warn("Generate updates and output as logs")
-    val logs = generateLogs(g)
+      logger.warn("Generate updates and output as logs")
+      val logs = generateLogs(g)
 
-    // If you want deterministic debugging, write the logs to a file and run using them
-    // val filename = "debug_logs-" + java.Instant.now().toString // Just so we dont overwrite anything
-    logs.serializeLogs(filename = "previous_run.tsv") // Write to file just in case
-    // val logs = sc.parallelize(readFromFile("previous_run.tsv"))
-
+      // If you want deterministic debugging, write the logs to a file and run using them
+      logger.warn("Writing logs to file")
+      logs.serializeLogs(filename = "previous_run.tsv") // Write to file just in case
+      logs
+    }
     logger.warn("Generate temporal model")
-    val snapshotModel = SnapshotDeltaObject.create(logs, SnapshotIntervalType.Count(5))
+    val snapshotModel = SnapshotDeltaObject.create(logs, SnapshotIntervalType.Count(10000))
 
     println(s"Number of vertices in the first snapshot ${snapshotModel.graphs.head.graph.vertices.count()}")
     println(s"Number of logs ${logs.count()}")
