9fe1872b001efa7cf47f94c4caa330ae62b0951e
Eivind Kopperud <eivind.kop@gmail.com>
2022-05-01 17:22:47 +0200

    feat+test: activatedentities

--- data-generation/src/test/scala/LandySpec.scala
+++ data-generation/src/test/scala/LandySpec.scala
@@ -1,8 +1,15 @@
 import factories.LandyGraphFactory
 import factories.LandyGraphFactory.createGraph
-import thesis.Landy
+import thesis.{Interval, Landy}
 import org.scalatest.flatspec.AnyFlatSpec
 import wrappers.SparkTestWrapper
+import factories.LogFactory
+import org.apache.spark.SparkContext
+import thesis.Entity.{EDGE, VERTEX}
+import utils.TimeUtils
+import utils.TimeUtils._
+
+import java.time.Instant
 
 
 class LandySpec extends AnyFlatSpec with SparkTestWrapper {
@@ -21,5 +28,37 @@ class LandySpec extends AnyFlatSpec with SparkTestWrapper {
 
     assert(graph.vertices.count() == 2)
     assert(graph.edges.count() == 1)
+  }
+
+  it can "query activated edges" in {
+    implicit val sparkContext: SparkContext = spark.sparkContext
+    val start = Instant.parse("2000-01-01T00:00:00Z")
+    val end = Instant.parse("2000-01-01T10:00:00Z")
+    val timestamps = TimeUtils.getDeterministicOrderedTimestamps(amount = 11, startTime = start, endTime = end)
+
+    val vertexLogs1 = LogFactory(startTime = timestamps(2), endTime = timestamps(8)).buildSingleSequence(VERTEX(1))
+    val vertexLogs2 = LogFactory(startTime = timestamps(5), endTime = timestamps(10)).buildSingleSequence(VERTEX(2))
+    val edgeLogs = LogFactory(startTime = timestamps(4), endTime = timestamps(8)).buildSingleSequence(EDGE(1, 1, 2))
+    val logs = (vertexLogs1 ++ vertexLogs2 ++ edgeLogs).sortBy(_.timestamp)
+    val logRDD = sparkContext.parallelize(logs)
+    val g = Landy(logRDD)
+
+    val intervalWithVertex1 = g.activatedEntities(Interval(timestamps.head, timestamps(3)))
+    val intervalWithVertex2 = g.activatedEntities(Interval(timestamps(5), timestamps(7)))
+    val intervalWithEdge = g.activatedEntities(Interval(timestamps(3), timestamps(5)))
+    val intervalWithV1andEdge = g.activatedEntities(Interval(timestamps.head, timestamps(4)))
+    val intervalWithV2andEdge = g.activatedEntities(Interval(timestamps(4), timestamps(7)))
+    val intervalWithAll = g.activatedEntities(Interval(timestamps.head, timestamps(10)))
+
+
+    assert(intervalWithVertex1._2.isEmpty() && intervalWithVertex1._1.count() == 1)
+
+    assert(intervalWithVertex2._1.count() == 1)
+
+    assert(intervalWithEdge._2.count() == 1)
+    assert(intervalWithV1andEdge._1.count() == 1, intervalWithV1andEdge._2.count() == 1)
+    assert(intervalWithV2andEdge._1.count() == 1, intervalWithV2andEdge._2.count() == 1)
+
+    assert(intervalWithAll._1.count() == 2 && intervalWithV1andEdge._2.count() == 1)
   }
 }
