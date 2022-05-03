ae7e71b7119e4a34b764d54f4082ce388550faa4
Eivind Kopperud <eivind.kop@gmail.com>
2022-05-03 09:47:34 +0200

    use timestamps from timeutils

--- data-generation/src/test/scala/utils/IntervalSpec.scala
+++ data-generation/src/test/scala/utils/IntervalSpec.scala
@@ -2,17 +2,9 @@ package utils
 
 import org.scalatest.flatspec.AnyFlatSpec
 import thesis.Interval
-
-import java.time.Instant
+import utils.TimeUtils.{t1, t2, t3, t4, t5}
 
 class IntervalSpec extends AnyFlatSpec {
-
-  val t1: Instant = Instant.parse("2000-01-01T00:00:00Z")
-  val t2: Instant = Instant.parse("2000-01-01T01:00:00Z")
-  val t3: Instant = Instant.parse("2000-01-01T02:00:00Z")
-  val t4: Instant = Instant.parse("2000-01-01T03:00:00Z")
-  val t5: Instant = Instant.parse("2000-01-01T04:00:00Z")
-
   "Intervals" can "overlap other intervals" in {
     assert(Interval(t1, t3).overlaps(Interval(t2, t4)))
     assert(Interval(t2, t4).overlaps(Interval(t1, t3)))
