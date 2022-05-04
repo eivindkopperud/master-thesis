81534b81493744ddda4e289f6d013f0b2d9bd1f0
Eivind Kopperud <eivind.kop@gmail.com>
2022-05-04 09:57:19 +0200

    example (to be removed before merge)

--- data-generation/src/main/scala/Main.scala
+++ data-generation/src/main/scala/Main.scala
@@ -1,6 +1,26 @@
-import thesis.PossibleWorkFlow
+import factories.LandyGraphFactory.createGraph
+import factories.LogFactory
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+import thesis.Entity.VERTEX
+import thesis.SparkConfiguration.getSparkSession
+import thesis.{Landy, PossibleWorkFlow}
+import utils.TimeUtils.t1
+import utils.{Benchmark, FileWriter, LogUtils, Writer}
 
 
 object Main extends App {
-  PossibleWorkFlow.run()
+  val spark: SparkSession = getSparkSession
+  implicit val sc: SparkContext = spark.sparkContext
+
+  val b = Benchmark(FileWriter(filename = "snapshot-landy"), textPrefix = "Snapshot", customColumn = "number of logs")
+  b.writeHeader
+  for (i <- 1 to 14) {
+    val numberOfLogs = scala.math.pow(2, i.toDouble).toInt
+    val logs = LogFactory().buildSingleSequenceWithDelete(VERTEX(1), updateAmount = numberOfLogs)
+    val graph = Landy(sc.parallelize(logs))
+    val timestamp = logs.head.timestamp
+    b.benchmarkSingle(graph.snapshotAtTime(timestamp), customColumnValue = numberOfLogs.toString)
+  }
+  b.close() // should be called if its a FileWriter
 }
