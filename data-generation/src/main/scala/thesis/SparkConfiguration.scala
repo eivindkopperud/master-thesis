f6553ad39d6f8b5b4d6600c1eec6ba6e0af17a29
Theodor René Carlsen <theodor.rene@gmail.com>
2022-05-10 11:28:04 +0200

    add environment file and util for getting env variables

--- data-generation/src/main/scala/thesis/SparkConfiguration.scala
+++ data-generation/src/main/scala/thesis/SparkConfiguration.scala
@@ -2,16 +2,18 @@ package thesis
 
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+import utils.UtilsUtils.getConfigSafe
 
 object SparkConfiguration {
 
   def getSparkSession: SparkSession = {
     val conf = new SparkConf()
       .setAppName("data-generation")
-      .setMaster("local[2]")
+      .setMaster(getConfigSafe("SparkConfigurationMaster").getOrElse("local[2]"))
       .set("spark.driver.memory", "2g")
     SparkSession.builder.config(conf).getOrCreate()
   }
+  //.setMaster("local[2]")
   //.setMaster("spark://spark:7077")
   // use the line above when making an uberjar
 }
--- data-generation/src/main/scala/utils/UtilsUtils.scala
+++ data-generation/src/main/scala/utils/UtilsUtils.scala
@@ -10,4 +10,8 @@ object UtilsUtils {
   def zipGraphs(g1: AttributeGraph, g2: AttributeGraph):
   (Seq[((VertexId, Attributes), (VertexId, Attributes))], Seq[(Edge[SnapshotEdgePayload], Edge[SnapshotEdgePayload])]) =
     (g1.vertices.zip(g2.vertices).collect(), g1.edges.zip(g2.edges).collect())
+
+  def getConfig(key: String): String = sys.env(key)
+
+  def getConfigSafe(key: String): Option[String] = sys.env.get(key)
 }
