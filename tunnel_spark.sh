2aef1ecc00a164b74bc4923aa7f4826d933ec43a
Theodor René Carlsen <theodor.rene@gmail.com>
2022-05-10 15:43:51 +0200

    fix bug that stumped me for 1+ hour

--- tunnel_spark.sh
+++ tunnel_spark.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-ssh -fN -L 7077:localhost:7077 -L 8080:localhost:8080 -L 8081:localhost:8081 -R 4040:localhost:4040 dif06.idi.ntnu.no
+ssh -fN -L 7077:localhost:7077 -L 8080:localhost:8080 -L 8081:localhost:8081 -L 4040:localhost:4040 dif06.idi.ntnu.no
