25cfb6c526ae9fcc472dc7e9c283995e0849c775
Eivind Kopperud <eivind.kop@gmail.com>
2022-05-10 09:12:00 +0200

    q3

---
+++ data-generation/src/main/scala/benchmarks/Q3.scala
@@ -0,0 +1,45 @@
+package benchmarks
+
+import org.apache.spark.graphx.Graph
+import thesis.DataSource.ContactsHyperText
+import thesis.DistributionType.UniformType
+import thesis.SnapshotIntervalType.Count
+import thesis.TopologyGraphGenerator.generateGraph
+import thesis.UpdateDistributions.{addGraphUpdateDistribution, generateLogs}
+import thesis.{Interval, Landy, SnapshotDelta, VERTEX}
+import utils.TimeUtils.secondsToInstant
+
+class Q3(
+          iterationCount: Int = 5,
+          customColumn: String = "average number of logs for each entity",
+          benchmarkSuffixes: Seq[String] = Seq("landy", "snapshot")
+        ) extends QueryBenchmark(iterationCount, customColumn, benchmarkSuffixes) {
+  val threshold = 40
+  val dataSource = ContactsHyperText
+  val distribution = (iteration: Int) => UniformType(1, 1 + 2 * iteration)
+  val timestamp = 1082148639L
+  val intervalDelta = 1000
+  val vertexId = 37
+
+  lazy val graph: Graph[Long, Interval] = {
+    generateGraph(threshold, dataSource).mapEdges(edge => {
+      val Interval(start, stop) = edge.attr
+      if (stop.isBefore(start)) Interval(stop, start) else Interval(start, stop)
+    })
+  }
+
+  override def execute(iteration: Int): Unit = {
+    val g = addGraphUpdateDistribution(graph, distribution(iteration))
+    val logs = generateLogs(g)
+
+    val landyGraph = Landy(logs)
+    val snapshotDeltaGraph = SnapshotDelta(logs, Count(intervalDelta))
+
+    val expectedLogPrEntity = (iteration + 1).toString
+
+    unpersist()
+    benchmarks(0).benchmarkAvg(landyGraph.getEntity(VERTEX(vertexId), timestamp), numberOfRuns = 5, customColumnValue = expectedLogPrEntity)
+    unpersist()
+    benchmarks(1).benchmarkAvg(snapshotDeltaGraph.getEntity(VERTEX(vertexId), timestamp), numberOfRuns = 5, customColumnValue = expectedLogPrEntity)
+  }
+}
