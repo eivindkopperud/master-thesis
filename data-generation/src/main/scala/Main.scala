f609ac361c62ff687cc5c406d3d284f87c4c33fa
Theodor René Carlsen <theodor.rene@gmail.com>
2022-05-11 13:34:36 +0200

    benchmark the whole scadoodle

--- data-generation/src/main/scala/Main.scala
+++ data-generation/src/main/scala/Main.scala
@@ -1,10 +1,12 @@
-import benchmarks.{Q1, Q2, Q3, Q4}
+import benchmarks.Q1
+import utils.{Benchmark, ConsoleWriter}
 
 object Main extends App {
   //getConfig("ENV_VARIABLES_ARE_SET") // Use this line if you want to make sure that env variabels are set
-  new Q1().run
-  new Q2().run
-  new Q3().run
-  new Q4().run
-
+  val b = new Benchmark(ConsoleWriter(), true, "Whole scadoodle")
+  b.benchmarkSingle(
+    {
+      new Q1().run
+    }
+  )
 }
