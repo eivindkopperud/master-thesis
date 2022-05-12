b7e33abe2b05e075c59dcd345348ae42c5f50dc2
Eivind Kopperud <eivind.kop@gmail.com>
2022-05-12 08:59:57 +0200

    update params

--- data-generation/src/main/scala/benchmarks/Q5.scala
+++ data-generation/src/main/scala/benchmarks/Q5.scala
@@ -7,18 +7,18 @@ import thesis.{Snapshot, SnapshotDelta, VERTEX}
 import java.time.Instant
 
 class Q5(
-          iterationCount: Int = 10,
+          iterationCount: Int = 20,
           customColumn: String = "logs since last materialization"
         ) extends QueryBenchmark(iterationCount, customColumn) {
-  val numLogs = 100
+  val numLogs = 100000
   val logs = LogFactory().buildSingleSequenceWithDelete(VERTEX(1), updateAmount = numLogs)
-  val snapshotGraph: SnapshotDelta = SnapshotDelta(sc.parallelize(logs), Count(10))
+  val snapshotGraph: SnapshotDelta = SnapshotDelta(sc.parallelize(logs), Count(1000))
 
   override def execute(iteration: Int): Unit = {
-    val materializationOffset = (iteration - 2) % 10
-    val timestamp = logs(numLogs / 2 + materializationOffset).timestamp
+    val materializationOffset = (iteration - 2) * 50
+    val timestamp = logs(numLogs / 2 - 1 + materializationOffset).timestamp
     sc.getPersistentRDDs.foreach(_._2.unpersist())
-    benchmarks(0).benchmarkAvg(doQuery(timestamp), numberOfRuns = 5, customColumnValue = materializationOffset.toString)
+    benchmarks(0).benchmarkAvg(doQuery(timestamp), numberOfRuns = 10, customColumnValue = materializationOffset.toString)
   }
 
   def doQuery(timestamp: Instant): Unit = {
