97dc6e586aefd6246c8aecef01af128653e4c8cf
Theodor René Carlsen <theodor.rene@gmail.com>
2022-05-13 13:58:08 +0200

    Add comments to env.sh

--- data-generation/env.sh
+++ data-generation/env.sh
@@ -11,7 +11,13 @@ export DISTRIBUTION_TYPE=2
 # 3 Guassian
 # 4 Zipf
 export DISTRIBUTION_PARAM1=1
+# 1 for Uniform
+# 1 for LogNormal
+# 4 for Gaussian
 export DISTRIBUTION_PARAM2=0.4
+# 1 for Uniform
+# 0.4 for LogNormal
+# 2 for Gaussian
 export TIMESTAMP=10
 export INTERVAL_DELTA=1000
 export VERTEX_ID=1
