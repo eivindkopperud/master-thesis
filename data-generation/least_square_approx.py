eb84ae61f2437c5d6735865a1e5a9565ecd911c4
Theodor René Carlsen <theodor.rene@gmail.com>
2022-05-28 18:23:04 +0200

    python least_square_approx <your folder>

--- data-generation/least_square_approx.py
+++ data-generation/least_square_approx.py
@@ -1,23 +1,38 @@
+import os
 from sys import argv
 import numpy as np
+from statistics import variance,mean
 # https://numpy.org/doc/stable/reference/generated/numpy.linalg.lstsq.html
 
-f = open(argv[1], "r")
-lines = f.readlines()
-x = []
-y = []
-for line in lines[1:]:
-    s = line.split(",")
-    x.append(int(s[-1]))
-    y.append(int(s[1]))
+path = argv[1]
+pwd = os.environ["PWD"]+"/"
+list_of_files = []
+for root, dirs, files in os.walk(pwd + path):
+    for file in files:
+        list_of_files.append(os.path.join(root,file))
 
-# Usikker på hva denne gjør, men den gjør at den funker
-A = np.vstack([x,np.ones(len(x))]).T
+list_of_stigningstall = []
+def calcStuff(filepath):
+    f = open(filepath, "r")
+    lines = f.readlines()
+    x = []
+    y = []
+    for line in lines[1:]:
+        s = line.strip().split(",")
+        x.append(int(s[-1]))
+        y.append(int(s[1]))
 
-m, c = np.linalg.lstsq(A,y, rcond=None)[0]
+    # Usikker på hva denne gjør, men den gjør at den funker
+    A = np.vstack([x,np.ones(len(x))]).T
 
-print(f"Stigningstall {m=}")
-print(f"restledd {c=}") # Husker ikke hva denne heter
+    m, c = np.linalg.lstsq(A,y, rcond=None)[0]
 
+    #print(f"{argv[1]}: ({m=}, {c=})")
+    print(m)
+    list_of_stigningstall.append(m)
+    f.close()
 
-f.close()
+for file in list_of_files:
+    calcStuff(file)
+
+print(f"Mean: {mean(list_of_stigningstall)}, Variance {variance(list_of_stigningstall)}")
